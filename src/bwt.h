23b4138b0bd782624aa52e9c6debe82cb9065c8f
Ricardo Čerljenko <ricardo.cerljenko@hotmail.com>
2016-04-21 16:13:51 +0200

    minor tweak in rld()

--- src/bwt.h
+++ src/bwt.h
@@ -124,14 +124,12 @@ static bwt_size_t rld(unsigned char* restrict data, const bwt_size_t n)
 	{
 		if(i < n - 2 && tmp_data[i] == tmp_data[i + 1])
 		{
-			for(j = 0; j < tmp_data[i + 2] + 2; j++) data[len++] = tmp_data[i];
+			for(j = 0; j <= tmp_data[i + 2]; j++) data[len++] = tmp_data[i];
 			j = 3;
 		}
-		else
-		{
-			data[len++] = tmp_data[i];
-			j = 1;
-		}
+		else j = 1;
+
+		data[len++] = tmp_data[i];
 	}
 
 	free(tmp_data);

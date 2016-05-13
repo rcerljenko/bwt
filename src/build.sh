96803c3fbdb3043e7ad21cf6656670cb34f6a813
Ricardo Čerljenko <ricardo.cerljenko@hotmail.com>
2016-05-14 00:01:01 +0200

    updated build command

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,6 @@
 CC=gcc
-CFLAGS="-Ofast -std=gnu11 -Wall -Wextra -s -m64 -march=native -Wl,-O1"
+CFLAGS="-Ofast -std=gnu11 -Wall -Wextra -s -m64 -march=native -Wl,-O1,-s"
 LIB_PATH=`pwd`
 
-$CC $CFLAGS -shared -fPIC -o libbwt.so libbwt.c
+$CC $CFLAGS -fPIC -shared -o libbwt.so libbwt.c
 $CC $CFLAGS -mfpmath=sse -pthread -Wl,-rpath=$LIB_PATH -L $LIB_PATH -o bwt bwt.c -lbwt

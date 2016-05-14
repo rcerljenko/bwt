e199b6f0d12630e840c3fc2f39915b51ae95ad22
Ricardo Čerljenko <ricardo.cerljenko@hotmail.com>
2016-05-14 09:58:39 +0200

    added -soname

--- src/build.sh
+++ src/build.sh
@@ -2,5 +2,5 @@ CC=gcc
 CFLAGS="-Ofast -std=gnu11 -Wall -Wextra -s -m64 -march=native -Wl,-O1,-s"
 LIB_PATH=`pwd`/libbwt
 
-$CC $CFLAGS -fPIC -shared -o $LIB_PATH/libbwt.so $LIB_PATH/libbwt.c
+$CC $CFLAGS -fPIC -Wl,-soname=libbwt.so -shared -o $LIB_PATH/libbwt.so $LIB_PATH/libbwt.c
 $CC $CFLAGS -mfpmath=sse -pthread -Wl,-rpath=$LIB_PATH -L $LIB_PATH -o bwt bwt.c -lbwt

6c0fae3a9971cdbf5d170baf31998b4b8a0920c6
Ricardo Čerljenko <ricardo.cerljenko@hotmail.com>
2016-05-15 08:46:06 +0200

    added -flto

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,5 @@
-CC=gcc
-CFLAGS="-Ofast -std=gnu11 -Wall -Wextra -s -m64 -march=native -Wl,-O1,-s"
+CC=gcc-6
+CFLAGS="-std=gnu11 -Wall -Wextra -s -m64 -march=native -Ofast -flto"
 LIB_PATH=`pwd`/libbwt
 
 $CC $CFLAGS -fPIC -Wl,-soname=libbwt.so -shared -o $LIB_PATH/libbwt.so $LIB_PATH/libbwt.c

c097ca9eece31c3902bf885b21f146373eda8517
Ricardo Čerljenko <ricardo.cerljenko@hotmail.com>
2016-06-14 14:29:38 +0200

    added thread_count in help

--- src/pbwt.c
+++ src/pbwt.c
@@ -516,6 +516,8 @@ static void show_statistics(const int signum)
 
 static void show_help(void)
 {
+	const unsigned short thread_count = get_threadcount();
+
 	fprintf(stderr, "%s - Portable multithreaded Burrows-Wheeler transform + Run Length Encoding compressor/decompressor.\n"
 		"Build date: " __DATE__ " @ " __TIME__ "\n\n"
 		"Usage: %s [<input_file>] [OPTIONS]\n\n"
@@ -530,14 +532,14 @@ static void show_help(void)
 		"\t-%c - Write output to STDOUT (ignored if valid -%c flag exists).\n"
 		"\t-%c - Decompression mode.\n"
 		"\t-%c - Show help and exit.\n"
-		"\t-%c <jobs_count> - Number of parallel jobs (threads). If less than 1 or greater than available threads, fallback to available threads.\n"
+		"\t-%c <1-%hu> - Number of parallel jobs (threads). If omitted, less than 1 or greater than available threads, fallback to available threads.\n"
 		"\t-%c <output_name> - Custom output filename (if omitted, output filename is input_file" FILE_EXT " in compression and without " FILE_EXT " in decompression).\n"
 		"\t\tIf given string ends with '/' (path delimiter), output is then given path + above rule.\n"
 		"\t-%c <%u-%u> - Compression preset level (ignored in decompression mode).\n"
 		"\t\tHigher presets give better compression ratio but decompression is considerably longer (if omitted or wrong value, preset is %u).\n"
 		"\t-%c - Remove input file after successful operation (ignored when input file is STDIN).\n"
 		"\t-%c - Verbose mode - show statistics after successful operation.\n"
-		, filename, filename, OUTPUT_FLAG, STDOUT_FLAG, STDOUT_FLAG, OUTPUT_FLAG, DEC_FLAG, HELP_FLAG, JOBS_FLAG, OUTPUT_FLAG, PRESET_FLAG, PRESET_MIN, PRESET_MAX, PRESET_DEF, REMOVE_FLAG, VERBOSE_FLAG);
+		, filename, filename, OUTPUT_FLAG, STDOUT_FLAG, STDOUT_FLAG, OUTPUT_FLAG, DEC_FLAG, HELP_FLAG, JOBS_FLAG, thread_count, OUTPUT_FLAG, PRESET_FLAG, PRESET_MIN, PRESET_MAX, PRESET_DEF, REMOVE_FLAG, VERBOSE_FLAG);
 }
 
 int main(const int argc, char **argv)

caa41c5f2343a002f20635c653a4da6c9ca1aadf
Ricardo Čerljenko <ricardo.cerljenko@hotmail.com>
2016-06-15 13:25:27 +0200

    removed temp build.sh script

--- src/build.sh
+++
@@ -1,6 +0,0 @@
-CC=gcc
-CFLAGS="-std=gnu11 -Wall -Wextra -s -march=native -Ofast -flto"
-LIB_PATH=`pwd`/libbwt
-
-$CC $CFLAGS -fPIC -Wl,-soname,libbwt.so -shared -o $LIB_PATH/libbwt.so $LIB_PATH/libbwt.c
-$CC $CFLAGS -Wl,-rpath=$LIB_PATH -L $LIB_PATH -o pbwt pbwt.c -pthread -lbwt

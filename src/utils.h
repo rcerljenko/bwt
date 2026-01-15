89aad3d32c94f7ec0e599bc15651f88e9fe656a7
Ricardo Cerljenko <ricardo@lloyds-digital.com>
2026-01-15 08:37:36 +0100

    move utils functions to utils.c

---
+++ src/utils.h
@@ -0,0 +1,26 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef _WIN32
+extern const char *optarg;
+extern unsigned short optind;
+#endif
+
+size_t get_filesize(FILE *const restrict fp);
+unsigned short get_threadcount(void);
+size_t get_memusage(void);
+int set_streams_to_binary_mode(void);
+#ifdef _WIN32
+short getopt(const unsigned short argc, char **const restrict argv, const char *const restrict args);
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif

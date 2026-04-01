3e37b1ae9961e33171b504aa2508b6854021a99a
Ricardo Cerljenko <ricardo@lloyds-digital.com>
2026-04-01 09:47:26 +0200

    added msvc memcheck

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -17,6 +17,11 @@ extern unsigned short optind;
 
 #ifdef __SANITIZE_ADDRESS__
 const char *__asan_default_options(void);
+
+const char *__asan_default_options(void)
+{
+	return "alloc_dealloc_mismatch=1:check_initialization_order=1:continue_on_error=2:strict_string_checks=1";
+}
 #endif
 
 size_t get_filesize(FILE *const restrict fp);

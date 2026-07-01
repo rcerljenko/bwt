8a641234617c7e6c20540016eeac480ccca5bbfb
Ricardo Cerljenko <ricardo@lloyds-digital.com>
2026-07-01 13:20:07 +0200

    minor tweak

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -13,16 +13,14 @@ extern "C" {
 #ifdef _WIN32
 extern const char *optarg;
 extern unsigned short optind;
+
+short getopt(const unsigned short argc, char **const restrict argv, const char *const restrict args);
 #endif
 
 size_t get_filesize(FILE *const restrict fp);
 unsigned short get_threadcount(void);
 size_t get_memusage(void);
 int set_streams_to_binary_mode(void);
-
-#ifdef _WIN32
-short getopt(const unsigned short argc, char **const restrict argv, const char *const restrict args);
-#endif
 
 #ifdef __cplusplus
 }
